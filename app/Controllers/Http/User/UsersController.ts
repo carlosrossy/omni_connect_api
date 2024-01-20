26f4729f1b40f1da27c63ea468dd848c9ee135c4
Carlos Eduardo <carlosrossy@bemol.com.br>
2024-01-20 16:45:56 -0400

    feat: userLogged

--- app/Controllers/Http/User/UsersController.ts
+++ app/Controllers/Http/User/UsersController.ts
@@ -8,6 +8,11 @@ import FullUserRegistrationValidator from 'App/Validators/FullUserRegistrationVa
 export default class UsersController {
   constructor(private readonly userRepository: UserRepository) {}
 
+  public async userLogged({ auth, response }: HttpContextContract) {
+    const userLogged = await auth.authenticate()
+    return response.ok(userLogged)
+  }
+
   public async fullUserRegistration({ params, auth, request, response }: HttpContextContract) {
     await auth.authenticate()
     const { userId } = params
--- start/user/user.ts
+++ start/user/user.ts
@@ -3,6 +3,7 @@ import Env from '@ioc:Adonis/Core/Env'
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
+  Route.get("/user-logged", "UsersController.userLogged");
   Route.post('/user/:userId/full-user-registration', 'UsersController.fullUserRegistration')
 })
   .prefix(Env.get('PREFIX'))
